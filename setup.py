8c250ae8013c2e3cdfefb88cbff48e46b9f8a9c1
Dustin Oprea <myselfasunder@gmail.com>
2014-05-22 23:43:34 -0400

    Reorganized layout for packaging.
    - Added setup.py .
    - Added ignore file.
    - Requirements update.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='tv_server',
+      version='0.2.0',
+      description="A API-controlled TV tuner management library.",
+      long_description="",
+      classifiers=[],
+      keywords='tv tuner',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/tvserver',
+      license='GPL 2',
+      packages=find_packages(exclude=['dev']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+            'web.py==0.37',
+      ],
+      scripts=[
+            'tv_server/resources/scripts/tv_start_backend',
+            'tv_server/resources/scripts/tv_start_webserver',
+      ],
+)
--- tv_server/tools/start_backend.py
+++
@@ -1,4 +0,0 @@
-from tv_server import log_config
-from tv_server.backend import server
-
-server.start_server()
